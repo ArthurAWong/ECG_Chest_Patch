85954ecd3846db4b06136809f32d28ddb42bb18e
Chen Zhou <zhouchenclyde@gmail.com>
2023-03-31 14:31:33 -0600

    Added demo code

---
+++ demo/demo_rand.py
@@ -0,0 +1,84 @@
+from PyQt5 import QtWidgets, uic, QtCore
+from pyqtgraph import PlotWidget
+import pyqtgraph as pg
+import sys
+import os
+import numpy as np
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    top_plot_data = np.zeros(100)
+    bottom_plot_data = np.random.normal(size=(10, 3))
+    axis = ('x','y','z')
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        uic.loadUi(os.path.join(os.path.dirname(__file__), "window.ui"), self)
+
+        screen_rect = QtWidgets.QDesktopWidget().screenGeometry(-1)
+        width = int(screen_rect.width() * 0.6)
+        frame_width = int(width)
+
+        height = int(screen_rect.height() * 0.6)
+        frame_height = int(height / 2)
+        self.setGeometry(0, 0, width, height)
+        self.setMinimumSize(QtCore.QSize(width, height))
+
+        central_layout = QtWidgets.QVBoxLayout(self.centralwidget)
+        central_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.top_plot_widget = pg.PlotWidget()
+        central_layout.addWidget(self.top_plot_widget)
+
+        self.top_plot_widget.setLabel("left", "Amplitude", units="V")
+        self.top_plot_widget.setLabel("bottom", "Time", units="s")
+
+        self.top_plot_curve = self.top_plot_widget.plot(self.top_plot_data)
+
+        self.bottom_plot_widget = pg.PlotWidget()
+        central_layout.addWidget(self.bottom_plot_widget)
+
+        self.bottom_plot_widget.setLabel("left", "Acceleration", units="m/s^2")
+        self.bottom_plot_widget.hideAxis('bottom')
+
+        self.bar_graph_item = pg.BarGraphItem(
+            x=range(3), height=[0, 0, 0], width=0.3, brush="r"
+        )
+        
+        self.bottom_plot_widget.addItem(self.bar_graph_item)
+
+        self.top_plot_widget.setMinimumSize(400, 400)
+        self.bottom_plot_widget.setMinimumHeight(frame_height)
+
+
+
+        self.timer = pg.QtCore.QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(50)
+
+    def update(self):
+        # Generate new data for the 2D plot
+        self.top_plot_data[:-1] = self.top_plot_data[1:]
+        self.top_plot_data[-1] = np.random.normal()
+        self.top_plot_curve.setData(self.top_plot_data)
+
+        heights = np.random.normal(size=(3,))
+        x = np.arange(3)
+        
+        self.bar_graph_item.setOpts(height=heights, x=range(3), brush='r', labels={i: f"Bar {i}" for i in self.axis})
+
+        
+
+if __name__ == "__main__":
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = MainWindow()  # Create an instance of our class
+
+    screen_size = QtWidgets.QDesktopWidget().screenGeometry(-1)
+    window_size = window.geometry()
+    window.move(
+        (screen_size.width() - window_size.width()) // 2,
+        (screen_size.height() - window_size.height()) // 2,
+    )
+    window.show()
+    sys.exit(app.exec_())
---
+++ demo/serial_data.py
@@ -0,0 +1,50 @@
+import serial.tools.list_ports
+import serial
+import time
+import matplotlib.pyplot as plt
+import random
+from matplotlib.animation import FuncAnimation
+from mpl_toolkits.mplot3d import Axes3D
+from functools import partial
+import re
+import warnings
+import numpy 
+warnings.simplefilter('ignore')
+ports = serial.tools.list_ports.comports()
+
+ecg_num = []
+timeArr = []
+counter = 0
+voltage_th = 500000
+x, y, z = [],[],[]
+xs, ys = [], []
+for index,port in enumerate(ports):
+    if "JLink" in port.description:
+        global ser
+        ser = serial.Serial(port.device,115200,xonxoff=True,rtscts=True) #115200 for bmd
+        ser.port = port.device
+        
+        print("Recognized JLink")
+        break
+    if index == len(ports) - 1:
+        print("JLink not found")
+
+
+
+def readPort():
+    data = ser.readline() # Read data from the UART device
+    data = data.decode('utf-8')
+    if not data:
+        pass
+    else:
+        retval = re.findall(r'^[-]?[0-9]+', data)
+        if retval == []:
+            retval = re.findall(r'-?\d+\.\d+|-?\d+',data)
+
+    return retval
+
+
+# while 1:
+#     print(readPort())
+
+
